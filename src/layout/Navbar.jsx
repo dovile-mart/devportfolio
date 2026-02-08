787da131f704bfa1773ee759c93d6b06c27e7426
dovile-mart <dovile.martinonyte@gmail.com>
2026-02-08 18:11:26 +0200

    Added navigation elements

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,5 +1,31 @@
+
+
+const navLinks = [
+    {href: "about", label: "About"},
+    {href: "projects", label: "Projects"},
+    {href: "experience", label: "Experience"},
+    {href: "testimonials", label: "Testimonials"},
+]
+
 export const Navbar = () => { 
     return (
-        <header></header>
+        <header>
+            <nav>
+                <a>
+                    DM<span>.</span>
+                </a>
+
+                <div>
+                    { /** Desktop Nav */}
+                    <div>
+                        {navLinks.map((link, index) => (
+                            <a href={link.href} key={index}>
+                                {link.label}
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </nav>
+        </header>
     );
 }
