5112d1a6e0740022ec3a448141e1717e01f58f2c
dovile-mart <dovile.martinonyte@gmail.com>
2026-06-25 12:09:03 +0300

    Profile image changed in Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -118,7 +118,7 @@ export const Hero = () => {
                           />
                           <div className="relative glass rounded-3xl p-2 glow-border">
                               <img
-                                  src="./profile.png"
+                                  src="./profile2.jpg"
                                   alt="Dovile"
                                   className="w-full aspect-[4/5] object-cover rounded-2xl"
                               />
